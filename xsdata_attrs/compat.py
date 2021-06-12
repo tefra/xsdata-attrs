10df00aff5e4dfe3138eb1a194c8e94e6dfa00b0
Chris <chris@komposta.net>
2021-06-12 22:27:56 +0300

    Add attrs generator and binding modules

---
+++ xsdata_attrs/compat.py
@@ -0,0 +1,87 @@
+from typing import Any
+from typing import Dict
+from typing import Generic
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Type
+from typing import TypeVar
+
+import attr
+from xsdata.formats.dataclass.compat import ClassType
+from xsdata.formats.dataclass.models.elements import XmlType
+
+T = TypeVar("T", bound=object)
+
+
+@attr.s
+class AnyElement:
+    """
+    Generic model to bind xml document data to wildcard fields.
+
+    :param qname: The element's qualified name
+    :param text: The element's text content
+    :param tail: The element's tail content
+    :param children: The element's list of child elements.
+    :param attributes: The element's key-value attribute mappings.
+    """
+
+    qname: Optional[str] = attr.ib(default=None)
+    text: Optional[str] = attr.ib(default=None)
+    tail: Optional[str] = attr.ib(default=None)
+    children: List[object] = attr.ib(factory=list, metadata={"type": XmlType.WILDCARD})
+    attributes: Dict[str, str] = attr.ib(
+        factory=dict, metadata={"type": XmlType.ATTRIBUTES}
+    )
+
+
+@attr.s
+class DerivedElement(Generic[T]):
+    """
+    Generic model wrapper for type substituted elements.
+
+    Example: eg. <b xsi:type="a">...</b>
+
+    :param qname: The element's qualified name
+    :param value: The wrapped value
+    :param type: The real xsi:type
+    """
+
+    qname: str = attr.ib()
+    value: T = attr.ib()
+    type: Optional[str] = attr.ib(default=None)
+
+
+class Attrs(ClassType):
+    @property
+    def any_element(self) -> Type:
+        return AnyElement
+
+    @property
+    def derived_element(self) -> Type:
+        return DerivedElement
+
+    def is_model(self, obj: Any) -> bool:
+        return attr.has(obj if isinstance(obj, type) else type(obj))
+
+    def get_fields(self, obj: Any) -> Tuple[Any, ...]:
+        if not isinstance(obj, type):
+            return self.get_fields(type(obj))
+
+        # Emulate dataclasses fields ordering
+        fields = {}
+        for b in obj.__mro__[-1:0:-1]:
+            if self.is_model(b):
+                for f in self.get_fields(b):
+                    fields[f.name] = f
+
+        for f in attr.fields(obj):
+            fields[f.name] = f
+
+        return tuple(fields.values())
+
+    def default_value(self, field: Any) -> Any:
+        if field.default is not attr.NOTHING:
+            return field.default
+
+        return None
