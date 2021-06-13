06dae7dcd12e820eb65179a39d4a8699223f6ee6
Chris <chris@komposta.net>
2021-06-13 23:27:59 +0300

    Adding unit tests

---
+++ tests/test_generator.py
@@ -0,0 +1,87 @@
+from xsdata.models.config import GeneratorConfig
+from xsdata.utils.testing import ClassFactory
+from xsdata.utils.testing import FactoryTestCase
+
+from xsdata_attrs.generator import AttrsGenerator
+
+
+class AttrsGeneratorTests(FactoryTestCase):
+    def setUp(self):
+        super().setUp()
+        config = GeneratorConfig()
+        self.generator = AttrsGenerator(config)
+
+    def test_render(self):
+        classes = [
+            ClassFactory.elements(2, package="foo"),
+            ClassFactory.elements(3, package="foo"),
+        ]
+
+        classes[0].attrs[0].restrictions.max_occurs = 3
+
+        iterator = self.generator.render(classes)
+
+        actual = [(out.path, out.title, out.source) for out in iterator]
+
+        expected = (
+            "import attr\n"
+            "from typing import List, Optional\n"
+            "\n"
+            '__NAMESPACE__ = "xsdata"\n'
+            "\n"
+            "\n"
+            "@attr.s\n"
+            "class ClassB:\n"
+            "    class Meta:\n"
+            '        name = "class_B"\n'
+            "\n"
+            "    attr_b: List[str] = attr.ib(\n"
+            "        factory=list,\n"
+            "        metadata={\n"
+            '            "name": "attr_B",\n'
+            '            "type": "Element",\n'
+            '            "max_occurs": 3,\n'
+            "        }\n"
+            "    )\n"
+            "    attr_c: Optional[str] = attr.ib(\n"
+            "        default=None,\n"
+            "        metadata={\n"
+            '            "name": "attr_C",\n'
+            '            "type": "Element",\n'
+            "        }\n"
+            "    )\n"
+            "\n"
+            "\n"
+            "@attr.s\n"
+            "class ClassC:\n"
+            "    class Meta:\n"
+            '        name = "class_C"\n'
+            "\n"
+            "    attr_d: Optional[str] = attr.ib(\n"
+            "        default=None,\n"
+            "        metadata={\n"
+            '            "name": "attr_D",\n'
+            '            "type": "Element",\n'
+            "        }\n"
+            "    )\n"
+            "    attr_e: Optional[str] = attr.ib(\n"
+            "        default=None,\n"
+            "        metadata={\n"
+            '            "name": "attr_E",\n'
+            '            "type": "Element",\n'
+            "        }\n"
+            "    )\n"
+            "    attr_f: Optional[str] = attr.ib(\n"
+            "        default=None,\n"
+            "        metadata={\n"
+            '            "name": "attr_F",\n'
+            '            "type": "Element",\n'
+            "        }\n"
+            "    )\n"
+        )
+
+        self.assertEqual(2, len(actual))
+        self.assertEqual(3, len(actual[1]))
+
+        self.assertEqual("foo.tests", actual[1][1])
+        self.assertEqual(expected, actual[1][2])
