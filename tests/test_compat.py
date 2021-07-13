650a8f714c84487f0247fdc5f55ac3caf8f36b3e
Chris <chris@komposta.net>
2021-07-13 16:43:50 +0300

    Working generator + bindings

---
+++ tests/test_compat.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from operator import attrgetter
+from unittest import TestCase
+
+from attr import Factory
+from xsdata.formats.dataclass.compat import class_types
+
+from tests.fixtures import TypeC
+from xsdata_attrs.bindings import XmlContext
+from xsdata_attrs.compat import AnyElement
+from xsdata_attrs.compat import Attrs
+from xsdata_attrs.compat import DerivedElement
+
+
+class AttrsTests(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.class_type = class_types.get_type("attrs")
+
+    def test_class_type(self):
+        self.assertIsInstance(self.class_type, Attrs)
+
+    def test_property_any_element(self):
+        self.assertIs(self.class_type.any_element, AnyElement)
+
+    def test_property_derived_element(self):
+        self.assertIs(self.class_type.derived_element, DerivedElement)
+
+    def test_is_model(self):
+        obj = AnyElement()
+        self.assertTrue(self.class_type.is_model(obj))
+        self.assertTrue(self.class_type.is_model(AnyElement))
+        self.assertFalse(self.class_type.is_model(XmlContext))
+
+    def test_get_fields(self):
+        obj = TypeC(one="a", two=1.1, three=False, four=datetime.now())
+        expected = ("one", "two", "three", "four", "any")
+        get_name = attrgetter("name")
+
+        from_obj = self.class_type.get_fields(obj)
+        self.assertEqual(expected, tuple(map(get_name, from_obj)))
+
+        from_type = self.class_type.get_fields(TypeC)
+        self.assertEqual(expected, tuple(map(get_name, from_type)))
+
+    def test_default_value(self):
+        fields = self.class_type.get_fields(TypeC)
+
+        self.assertIsNone(self.class_type.default_value(fields[0]))
+        self.assertIsNone(self.class_type.default_value(fields[1]))
+        self.assertTrue(self.class_type.default_value(fields[2]))
+        self.assertEqual(list, self.class_type.default_value(fields[3]))
--- xsdata_attrs/generator.py
+++ xsdata_attrs/generator.py
@@ -1,7 +1,83 @@
+from dataclasses import dataclass
+from typing import Any
+from typing import Dict
+from typing import List
+from typing import Optional
+
+from xsdata.codegen.models import Attr
+from xsdata.formats.dataclass.filters import Filters
 from xsdata.formats.dataclass.generator import DataclassGenerator
+from xsdata.models.config import GeneratorConfig
+from xsdata.models.config import OutputFormat
+from xsdata.models.mixins import attribute
 
 
 class AttrsGenerator(DataclassGenerator):
-    """attrs generator."""
+    """Python attrs classes code generator."""
 
-    pass
+    @classmethod
+    def init_filters(cls, config: GeneratorConfig) -> Filters:
+        return AttrsFilters(config)
+
+
+@dataclass
+class AttrsOutputFormat(OutputFormat):
+    slots: bool = attribute(default=True)
+    cache_hash: bool = attribute(default=False)
+    kw_only: bool = attribute(default=False)
+
+
+class AttrsFilters(Filters):
+    FACTORY_KEY = "factory"
+    DEFAULT_KEY = "default"
+
+    @classmethod
+    def build_class_annotation(cls, format: OutputFormat) -> str:
+        args = []
+
+        if not format.repr:
+            args.append("repr=False")
+        if not format.eq:
+            args.append("eq=False")
+        if format.order:
+            args.append("order=True")
+        if format.unsafe_hash:
+            args.append("unsafe_hash=True")
+        if format.frozen:
+            args.append("frozen=True")
+
+        if isinstance(format, AttrsOutputFormat):
+            if format.slots:
+                args.append("slots=True")
+            if format.kw_only:
+                args.append("kw_only=True")
+            if format.cache_hash:
+                args.append("cache_hash=True")
+
+        return f"@attr.s({', '.join(args)})" if args else "@attr.s"
+
+    def field_definition(
+        self,
+        attr: Attr,
+        ns_map: Dict,
+        parent_namespace: Optional[str],
+        parents: List[str],
+    ) -> str:
+        result = super().field_definition(attr, ns_map, parent_namespace, parents)
+
+        return result.replace("field(", "attr.ib(")
+
+    def field_default_value(self, attr: Attr, ns_map: Optional[Dict] = None) -> Any:
+        result = super().field_default_value(attr, ns_map)
+        if isinstance(self.format, AttrsOutputFormat):
+            if self.format.kw_only and not attr.restrictions.is_optional:
+                return False
+
+        return result
+
+    @classmethod
+    def build_import_patterns(cls) -> Dict[str, Dict]:
+        patterns = {"attr": {"__module__": [" attr.ib", "@attr.s"]}}
+
+        patterns.update(super().build_import_patterns())
+        return patterns
