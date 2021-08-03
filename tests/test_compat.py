cf14fe4a20c47a65facfe8a1f93dd495baeb4d24
Chris <chris@komposta.net>
2021-08-03 21:04:14 +0300

    Increase coverage

--- tests/test_compat.py
+++ tests/test_compat.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from operator import attrgetter
 from unittest import TestCase
 
+from xsdata.exceptions import XmlContextError
 from xsdata.formats.dataclass.compat import class_types
 
 from tests.fixtures.common import TypeC
@@ -31,6 +32,17 @@ class AttrsTests(TestCase):
         self.assertTrue(self.class_type.is_model(AnyElement))
         self.assertFalse(self.class_type.is_model(XmlContext))
 
+    def test_verify_model(self):
+        obj = AnyElement()
+        self.class_type.verify_model(obj)
+
+        with self.assertRaises(XmlContextError) as cm:
+            self.class_type.verify_model(int)
+
+        self.assertEqual(
+            "Type '<class 'int'>' is not an attrs model.", str(cm.exception)
+        )
+
     def test_get_fields(self):
         obj = TypeC(one="a", two=1.1, three=False, four=datetime.now())
         expected = ("one", "two", "three", "four", "any")
@@ -49,3 +61,19 @@ class AttrsTests(TestCase):
         self.assertIsNone(self.class_type.default_value(fields[1]))
         self.assertTrue(self.class_type.default_value(fields[2]))
         self.assertEqual(list, self.class_type.default_value(fields[3]))
+
+    def test_default_choice_value(self):
+        choice = {}
+        self.assertIsNone(self.class_type.default_choice_value(choice))
+
+        choice["factory"] = 1
+        self.assertIsNone(self.class_type.default_choice_value(choice))
+
+        choice["default"] = 1
+        self.assertEqual(1, self.class_type.default_choice_value(choice))
+
+        choice["factory"] = lambda: 2
+
+        default = self.class_type.default_choice_value(choice)
+        self.assertTrue(callable(default))
+        self.assertEqual(2, default())
